6d1f4e4657eea771d8b69b418db343eb36679a02
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-28 13:18:11 -0400

    Rename churn_rate to member_churn_rate in query

--- sql/analysis_queries/active_member_vs_churn_rate.sql
+++ sql/analysis_queries/active_member_vs_churn_rate.sql
@@ -2,6 +2,6 @@
 -- Purpose: understand whether engagement (being an active member) reduces churn
 
 SELECT
-  ROUND(AVG(exited),2) AS churn_rate
+  ROUND(AVG(exited),2) AS member_churn_rate
   FROM `named-foundry-494619-d0.customer_churn.customers_cleaned`
 WHERE is_active_member = 1
