262c3e6472ea83c3eba38992adcf99676d8e90a5
Sebastian Hopgood <sebastianhopgood@gmail.com>
2026-04-28 13:03:00 -0400

    Add churn rate calculation for active members
    This query calculates the churn rate for active members to analyze the impact of engagement on churn.

---
+++ sql/analysis_queries/active_member_vs_churn_rate.sql
@@ -0,0 +1,7 @@
+-- Calculates churn rate for active members only
+-- Purpose: understand whether engagement (being an active member) reduces churn
+
+SELECT
+  ROUND(AVG(exited),2) AS churn_rate
+  FROM `named-foundry-494619-d0.customer_churn.customers_cleaned`
+WHERE is_active_member = 1
